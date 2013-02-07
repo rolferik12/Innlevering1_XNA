20eedd2462912b9a6db24e0f78c560c280a3bad0
Jonathan Wettby <wetjon12@nith.no>
2013-02-07 18:38:42 +0100

    Diamonds lasts forevva!

--- Innlevering1_XNA/Innlevering1_XNA/Ladybug.cs
+++ Innlevering1_XNA/Innlevering1_XNA/Ladybug.cs
@@ -13,6 +13,7 @@ namespace Innlevering1_XNA
         Texture2D DM2;
         Texture2D DM3;
         Texture2D Enemy_Bug_Troll;
+        Texture2D Enemy_Bug_Troll_Down;
         Vector2 bugPosition;
         Vector2 bugSize;
         Vector2 DMPosition;
@@ -22,16 +23,15 @@ namespace Innlevering1_XNA
         int teller = 20;
         int Score = 0;
         Random rnd = new Random();
+        float timer = float.NaN;
 
         public void BugNewPosition() 
         {
-            showDM = false;
             teller = 20;
             bugPosition = possiblePositions[rnd.Next(5)];
         }
         public void DMNewPossition()
         {
-            showDM = true;
             DMPosition = bugPosition;
         }
 
@@ -40,6 +40,7 @@ namespace Innlevering1_XNA
             float x = GameStatus.windowBorder.X / 7;
             possiblePositions = new Vector2[] { new Vector2(x * 2, 0), new Vector2(x * 3, 0), new Vector2(x * 4, 0), new Vector2(x * 2, 0), new Vector2(x * 6, 0) };
             Enemy_Bug_Troll = GameStatus.Content.Load<Texture2D>("Character/Enemy_Bug_roll");
+            Enemy_Bug_Troll_Down = GameStatus.Content.Load<Texture2D>("Character/Enemy BugTrollClick");
             DM1 = GameStatus.Content.Load<Texture2D>("Other/Gem Blue");
             DM2 = GameStatus.Content.Load<Texture2D>("Other/Gem Green");
             DM3 = GameStatus.Content.Load<Texture2D>("Other/Gem Orange");
@@ -51,35 +52,57 @@ namespace Innlevering1_XNA
         }
         public void Update()
         {
-            if (GameStatus.MouseNewDown && GameStatus.Collide(bugPosition, bugSize, GameStatus.MousePosition, Vector2.One) && showDM == false)
+            if (!timer.Equals(float.NaN))
             {
-                teller--;
-                if (teller == 0)
+                timer -= GameStatus.GameTimeInSec;
+                if (timer < 0) 
                 {
-                    DMNewPossition();
+                    timer = float.NaN;
+                    showDM = !showDM;
                 }
             }
-            else if (GameStatus.MouseNewDown && GameStatus.Collide(DMPosition, DMSize, GameStatus.MousePosition, Vector2.One) && showDM == true)
+            else
             {
-                Score++;
-                if (Score == 3)
+                if (GameStatus.MouseNewDown && GameStatus.Collide(bugPosition, bugSize, GameStatus.MousePosition, Vector2.One) && showDM == false)
                 {
-                    //end game *Du har vunnet TROLOLOLOL!*
-                }
-                else
-                {
-                    BugNewPosition();
-                }
+                    teller--;
+                    if (teller == 0)
+                    {
+                        DMNewPossition();
+                        timer = 0.5f;
+                    }
 
+                }
+                else if (GameStatus.MouseNewDown && GameStatus.Collide(DMPosition, DMSize, GameStatus.MousePosition, Vector2.One) && showDM == true)
+                {
+                    Score++;
+                    if (Score == 3)
+                    {
+                        //end game *Du har vunnet TROLOLOLOL!*
+                    }
+                    else
+                    {
+
+                        BugNewPosition();
+                        timer = 5;
+                    }
+                } 
             }
         }
         public void Draw()
         {
-            if (showDM == true)
+            if (timer.Equals(float.NaN))
             {
-                GameStatus.SpriteBatch.Draw(DM1, new Rectangle((int)DMPosition.X, (int)DMPosition.Y, (int)DMSize.X, (int)DMSize.Y), null, Color.White, 0, Vector2.Zero, SpriteEffects.None, 0);
+                if (showDM == true)
+                {
+                    GameStatus.SpriteBatch.Draw(DM1, new Rectangle((int)DMPosition.X, (int)DMPosition.Y, (int)DMSize.X, (int)DMSize.Y), null, Color.White, 0, Vector2.Zero, SpriteEffects.None, 0.2f);
+                }
+                else
+                {
+                    if (GameStatus.MouseDown && GameStatus.Collide(bugPosition, bugSize, GameStatus.MousePosition, Vector2.One)) GameStatus.SpriteBatch.Draw(Enemy_Bug_Troll_Down, new Rectangle((int)bugPosition.X, (int)bugPosition.Y, (int)bugSize.X, (int)bugSize.Y), null, Color.White, 0, Vector2.Zero, SpriteEffects.None, 0.2f);
+                    else GameStatus.SpriteBatch.Draw(Enemy_Bug_Troll, new Rectangle((int)bugPosition.X, (int)bugPosition.Y, (int)bugSize.X, (int)bugSize.Y), null, Color.White, 0, Vector2.Zero, SpriteEffects.None, 0.2f);
+                } 
             }
-            else GameStatus.SpriteBatch.Draw(Enemy_Bug_Troll, new Rectangle((int)bugPosition.X, (int)bugPosition.Y, (int)bugSize.X, (int)bugSize.Y), null, Color.White, 0, Vector2.Zero, SpriteEffects.None, 0);
         }
     }
 }
